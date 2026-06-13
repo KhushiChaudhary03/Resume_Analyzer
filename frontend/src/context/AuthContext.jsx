5f985310f477dfc78853e8d6a8b79f1492d1f489
KhushiChaudhary03 <khushichaudhary0310@gmail.com>
2026-06-13 10:58:21 +0530

    changes for deployment

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext(null);
 
-const API = "http://127.0.0.1:5000/api";
+const API = import.meta.env.VITE_API_URL;
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
